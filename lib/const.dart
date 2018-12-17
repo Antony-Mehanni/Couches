ea336074133edb8ad0d63795b2628967309d6ab2
Antony <antony.mehanni@bhpbilliton.com>
2018-12-18 07:26:29 +0800

    Initial commit v0.1

---
+++ lib/const.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+final themeColor = new Color(0xff8c42f4); /* Orange 0xfff5a623*/
+final primaryColor = new Color(0xff2A1548); /*Dark blue 0xff203152 */
+final greyColor = new Color(0xffaeaeae); /* Grey  0xffaeaeae*/
+final greyColor2 = new Color(0xffE8E8E8); /* red 0xffE8E8E8 */
